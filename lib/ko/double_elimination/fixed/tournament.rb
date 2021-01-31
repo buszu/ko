95444738ac0b40b2e5628f032ac9cae10ac895f0
Krzysztof Buszewicz <krzysztof.buszewicz@gmail.com>
2021-01-31 14:45:29 +0100

    getters for fixed tournamen

--- lib/ko/double_elimination/fixed/tournament.rb
+++ lib/ko/double_elimination/fixed/tournament.rb
@@ -4,10 +4,41 @@ module Ko
   module DoubleElimination
     module Fixed
       class Tournament
+        extend Forwardable
+
         attr_reader :body
+
+        def_delegators :body, :[]
+        alias get []
 
         def initialize(tournament)
           @body = tournament.as_json
+        end
+
+        def size
+          get(:size)
+        end
+
+        def finals
+          get(:finals)
+        end
+
+        def matches_graph
+          get(:matches_graph)
+        end
+
+        def matches_queue(by = nil)
+          queue = get(:matches_queue)
+          return queue if by.nil?
+
+          queue[by]
+        end
+
+        def rounds(purpose = nil)
+          rounds = get(:rounds)
+          return rounds if purpose.nil?
+
+          rounds[purpose]
         end
       end
     end
